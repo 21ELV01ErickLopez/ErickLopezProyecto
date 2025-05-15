158ffd06bf6fd56f04da7593cf7cd82d7642641d
21ELV01ErickLopez <e.lopez.1cdb@gmail.com>
2025-05-14 22:00:15 -0600

    Update Form2.cs

--- ErickLopezProyecto/Form2.cs
+++ ErickLopezProyecto/Form2.cs
@@ -26,7 +26,6 @@ namespace ErickLopezProyecto
                 double b = Convert.ToDouble(textBox2.Text);
                 double c = Convert.ToDouble(textBox3.Text);
 
-                // Calcular el discriminante
                 double discriminante = b * b - 4 * a * c;
 
                 if (a == 0)
@@ -37,20 +36,17 @@ namespace ErickLopezProyecto
 
                 if (discriminante > 0)
                 {
-                    // Dos soluciones reales diferentes
                     double x1 = (-b + Math.Sqrt(discriminante)) / (2 * a);
                     double x2 = (-b - Math.Sqrt(discriminante)) / (2 * a);
                     textBox4.Text = $"Las soluciones son: x1 = {x1}, x2 = {x2}";
                 }
                 else if (discriminante == 0)
                 {
-                    // Una solución real (doble)
                     double x = -b / (2 * a);
                     textBox4.Text = $"La solución es: x = {x}";
                 }
                 else
                 {
-                    // Soluciones complejas
                     double realParte = -b / (2 * a);
                     double imaginarioParte = Math.Sqrt(-discriminante) / (2 * a);
                     textBox4.Text = $"Las soluciones son complejas: x1 = {realParte} + {imaginarioParte}i, x2 = {realParte} - {imaginarioParte}i";
